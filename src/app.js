6a46b6fb36e428116ac6061c3ad2dc0b123cb19c
p.uran <patryk.uran@plej.pl>
2019-02-14 20:12:01 +0100

    finish lesson 29

--- src/app.js
+++ src/app.js
@@ -1,61 +1,94 @@
-const app = {
-    title: "Indecision App",
-    subtitle: "A helping hand in making up your mind!",
-    options: ["One", "two", "three"]
-}
+class IndecisionApp extends React.Component {
+    render() {
+    const title = 'Indecision';
+    const subtitle = 'Put your life in the hands of computer';
+    const options = ['Option one', 'Option two', 'Option three', 'Option five', 'Option six'];
 
-const renderDOM = function() {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <h2>{app.subtitle}</h2>}
-            <p>{(app.options && app.options.length > 0) ? "Here are your options" : "No options"}</p>
-            <p>{app.options.length}</p>
-
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should i do?</button>
-            <button onClick={removeAll}>Remove All Options</button>
-
-            <ol>
-                {
-                    app.options.map((option) => <li key={option}>{option}</li>)
-                }
-            </ol>
-    
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option" />
-                <button>
-                    Add Option
-                </button>
-            </form>
-        </div>
-    );
-
-    ReactDOM.render(template, root);
-}
-
-const onFormSubmit = (event) => {
-    event.preventDefault();
-    const option = event.target.elements.option.value
-    
-    if(option) {
-        app.options.push(option)
-        event.target.elements.option.value = ""
-        renderDOM();
+        return (
+            <div>
+                <Header title={title} subtitle={subtitle}/>
+                <Action />
+                <Options options={options}/>
+                <AddOption />
+            </div>
+        )
     }
 }
 
-const removeAll = () => {
-    app.options= [];
-    renderDOM();
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.title}</h1>
+                <h2>{this.props.subtitle}</h2>
+            </div>
+        )
+    }
 }
 
-const onMakeDecision = () => {
-    const randomNumber = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNumber];
-    alert(option)
-
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
 }
 
-const root = document.getElementById('root');
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+            <RemoveAll />
+                <ul>Here are your {this.props.options.length} options:
+                {
+                    this.props.options.map((option) => <Option key={option} optionText={option}/>)
+                }
+                </ul>
+            </div>
+        )
+    }
+}
 
-renderDOM();
+class Option extends React.Component {
+    render() {
+        return (
+            <li key={this.props.key}>Option: {this.props.optionText}</li>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault()
+        const value = e.target.elements.option.value.trim();
+        console.log(value);
+    }
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleAddOption}>
+                    <legend>Add option:</legend>
+                    <input type="text" name="option"></input>
+                    <button type="submit">Add Option</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+class RemoveAll extends React.Component {
+    handleRemoveAll() {
+        alert("lol")
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleRemoveAll}>Remove All</button>
+        )
+    }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('root'))
