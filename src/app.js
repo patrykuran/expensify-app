460ce1cdc5b353cbb2c9ec422ee34c28561a6341
p.uran <patryk.uran@plej.pl>
2019-02-25 18:34:50 +0100

    finish lesson 80

--- src/app.js
+++ src/app.js
@@ -1,34 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
+
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-const ExpenseDashboard = () => (
-    <div>Expense Dashboard</div>
-);
-
-const AddExpensePage = () => (
-    <div>Add Expense</div>
-);
-
-const EditExpense = () => (
-    <div>Edit Expense</div>
-);
-
-const HelpPage = () => (
-    <div>Help Page</div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <div>
-            <Route path="/" component={ExpenseDashboard} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpense} />
-            <Route path="/help" component={HelpPage} />
-        </div>
-    </BrowserRouter>
-)
-
-ReactDOM.render(routes, document.getElementById('root'))
+ReactDOM.render(<AppRouter />, document.getElementById('root'))
